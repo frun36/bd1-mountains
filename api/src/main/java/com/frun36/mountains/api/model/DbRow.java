3a9a555bc9f8877af7a0e06e73b601d3fb39b69f
frun36 <franek.urbanski@gmail.com>
2024-12-23 22:05:08 +0100

    Resolve nulls being replaced by 0

--- api/src/main/java/com/frun36/mountains/api/model/DbRow.java
+++ api/src/main/java/com/frun36/mountains/api/model/DbRow.java
@@ -5,43 +5,43 @@ import java.sql.SQLException;
 import java.util.Map;
 
 public interface DbRow {
-    Map<String, Object> asMap();
+        Map<String, Object> asMap();
 
-    public static DbRow parseResultSet(ResultSet rs, String table) throws SQLException {
-        return switch (table) {
-            case "point" -> new Point(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getInt("altitude"),
-                    rs.getString("type"));
+        public static DbRow parseResultSet(ResultSet rs, String table) throws SQLException {
+                return switch (table) {
+                        case "point" -> new Point(
+                                        rs.getObject("id", Integer.class),
+                                        rs.getString("name"),
+                                        rs.getObject("altitude", Integer.class),
+                                        rs.getString("type"));
 
-            case "app_user" -> new AppUser(
-                    rs.getInt("id"),
-                    rs.getString("username"),
-                    rs.getString("password"),
-                    rs.getInt("total_got_points"));
+                        case "app_user" -> new AppUser(
+                                        rs.getObject("id", Integer.class),
+                                        rs.getString("username"),
+                                        rs.getString("password"),
+                                        rs.getObject("total_got_points", Integer.class));
 
-            case "route" -> new Route(
-                    rs.getInt("id"),
-                    rs.getString("name"),
-                    rs.getInt("user_id"),
-                    rs.getTimestamp("time_modified"));
+                        case "route" -> new Route(
+                                        rs.getObject("id", Integer.class),
+                                        rs.getString("name"),
+                                        rs.getObject("user_id", Integer.class),
+                                        rs.getTimestamp("time_modified"));
 
-            case "route_trail" -> new RouteTrail(
-                    rs.getInt("id"),
-                    rs.getInt("route_id"),
-                    rs.getInt("trail_id"),
-                    rs.getInt("prev_id"),
-                    rs.getInt("next_id"));
+                        case "route_trail" -> new RouteTrail(
+                                        rs.getObject("id", Integer.class),
+                                        rs.getObject("route_id", Integer.class),
+                                        rs.getObject("trail_id", Integer.class),
+                                        rs.getObject("prev_id", Integer.class),
+                                        rs.getObject("next_id", Integer.class));
 
-            case "trail" -> new Trail(
-                    rs.getInt("id"),
-                    rs.getInt("start_point_id"),
-                    rs.getInt("end_point_id"),
-                    rs.getInt("got_points"),
-                    rs.getString("color"));
-            
-            default -> null;
-        };
-    }
+                        case "trail" -> new Trail(
+                                        rs.getObject("id", Integer.class),
+                                        rs.getObject("start_point_id", Integer.class),
+                                        rs.getObject("end_point_id", Integer.class),
+                                        rs.getObject("got_points", Integer.class),
+                                        rs.getString("color"));
+
+                        default -> null;
+                };
+        }
 }
